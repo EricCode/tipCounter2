de2ebb0dfeb395226e155100acabf501f028ff64
ericcode <eric78723@gmail.com>
2016-07-18 14:06:23 +0800

    Initial Commit

---
+++ tipCalculate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculate
+//
+//  Created by EricYang on 2016/7/18.
+//  Copyright © 2016年 EricYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
